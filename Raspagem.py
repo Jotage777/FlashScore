ece5e202b3f63011fd726c37ab29bfaf9178c19a
Jotage777 <gabrieloliveira_pb@hotmail.com>
2021-12-06 23:13:40 -0300

    Jogos ok

--- Raspagem.py
+++ Raspagem.py
@@ -1,56 +1,50 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from time import sleep
+
+from selenium.common.exceptions import NoSuchElementException
+
 import Banco_Dados
 
 def raspagemDados(navegador,liga):
     # Acessando os jogos da Liga escolhida
-    # A variável todos realiza um click na pagina para carregar todos os jogos da pagina
+
     while True:
         try:
-           todos = navegador.find_element_by_xpath('/html/body/div[5]/div[1]/div/div[1]/div[2]/div[4]/div[2]/div[1]/div[1]/div/div/a').click()
+            carregar= navegador.find_element_by_xpath('/html/body/div[5]/div[1]/div/div[1]/div[2]/div[4]/div[2]/div[1]/div[1]/div/div/a')
+            while True:
+                try:
+                  carregar.click()
+                  print('Carregando todos os jogos')
 
-        except:
-            print(f'\nErro na pagina html...')
-            sleep(1)
-
-        else:
-          break
-
-    #A variável jogos dar acesso ao html aonde os jogos estão
-
+                except :
+                    print(f'\nErro na pagina html...')
+                    break
+        except NoSuchElementException:
+            break
     jogos = navegador.find_element_by_xpath('/html/body/div[5]/div[1]/div/div[1]/div[2]/div[4]/div[2]/div[1]/div[1]/div')
     html_content = jogos.get_attribute('outerHTML')
     soup = BeautifulSoup(html_content, 'html.parser')
-    # a Variavel casa o resultado do time da casa
-    casa = soup.find_all('div', class_='event__score event__score--home')
-    # a Variavel fora o resultado do time de fora
-    fora = soup.find_all('div', class_='event__score event__score--away')
-    cont=0
-    ultimo=0
+    cont = 0
     #Pegando o id de cada jogo
-    for rodada in soup.find_all('div', attrs={"class": "event__match event__match--static event__match--twoLine"}):
-        if ultimo==9:
-            cont+=1
-            ultimo=0
-        #Id de cada partida
+    for rodada in soup.find_all('div', attrs={"class": "event__match event__match--static event__match--twoLine"} ):
+         #Id de cada partida
         id_jogo=rodada['id']
         id_j = id_jogo[4:]
         #mudando o id na url
         caminho='https://www.flashscore.com.br/jogo/'+id_j+'/#resumo-de-jogo/estatisticas-de-jogo/0'
-        raspagem_stats(caminho, casa[cont].get_text(), fora[cont].get_text(), id_j,liga)
-        cont+=1
-        ultimo+=1
-    cont=9
+        raspagem_stats(caminho,  id_j,liga)
+
+
     #Como a div da pagina para o ultimo jogo de cada rodada é diferente, temos que criar esse for para raspar os dados da ultima partida
     for rod in soup.find_all('div', attrs={"class": "event__match event__match--static event__match--last event__match--twoLine"}):
         id_jog = rod['id']
         id_jj = id_jog[4:]
         caminho1 = 'https://www.flashscore.com.br/jogo/' + id_jj + '/#resumo-de-jogo/estatisticas-de-jogo/0'
-        raspagem_stats(caminho1, casa[cont].get_text(), fora[cont].get_text(), id_jj,liga)
-        cont+=10
+        raspagem_stats(caminho1, id_jj,liga)
 
-def raspagem_stats(url,home, away, id_jogo,liga):
+
+def raspagem_stats(url,id_jogo,liga):
     nave=webdriver.Chrome()
     nave.get(url)
 
@@ -59,6 +53,12 @@ def raspagem_stats(url,home, away, id_jogo,liga):
     soup2 = BeautifulSoup(html_2, 'html.parser')
     v=soup2.find_all('a')
     rodada = v[4]
+    res =soup2.find_all('div', class_='detailScore__wrapper')
+    ress = res[0].get_text()
+    for i in range(len(ress)):
+        if ress[i] == '-':
+            res_casa = int(ress[:i])
+            res_fora = int(ress[i+1:])
 
     # Rodada
     # O nome de cada time
@@ -98,7 +98,7 @@ def raspagem_stats(url,home, away, id_jogo,liga):
                 stats_partidas.append(0)
                 stats_partidas.append(0)
 
-    Banco_Dados.add_jogos(liga, id_jogo,times[0].get_text(),home,times[1].get_text(),away,data_hora[0].get_text(),rodada.get_text(),stats_partidas[0],stats_partidas[1],stats_partidas[2],stats_partidas[3],stats_partidas[4],
+    Banco_Dados.add_jogos(liga, id_jogo,times[0].get_text(),res_casa,times[1].get_text(),res_fora,data_hora[0].get_text(),rodada.get_text(),stats_partidas[0],stats_partidas[1],stats_partidas[2],stats_partidas[3],stats_partidas[4],
                           stats_partidas[5],stats_partidas[6],stats_partidas[7],stats_partidas[8],stats_partidas[9],stats_partidas[10],stats_partidas[11],stats_partidas[12],stats_partidas[13],stats_partidas[14],
                           stats_partidas[15],stats_partidas[16],stats_partidas[17],stats_partidas[18],stats_partidas[19],stats_partidas[20],stats_partidas[21],stats_partidas[22],stats_partidas[23],stats_partidas[24],
                           stats_partidas[25],stats_partidas[26],stats_partidas[27],stats_partidas[28],stats_partidas[29],stats_partidas[30],stats_partidas[31],stats_partidas[32],stats_partidas[33],stats_partidas[34],stats_partidas[35])
